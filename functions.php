9e3e9da2267b75db540afd097e0e2504d58e656a
nosorrow <plamenorama@gmail.com>
2018-10-16 20:33:29 +0300

    theme - 1

--- functions.php
+++ functions.php
@@ -19,3 +19,23 @@ function my_action ()
     die;
 
 }
+
+
+/*add_action( 'wp_footer', 'my_action_javascript' ); // Write our JS below here
+
+function my_action_javascript() { */?><!--
+    <script type="text/javascript" >
+        jQuery(document).ready(function($) {
+
+            var data = {
+                'action': 'my_action',
+                'whatever': 1234
+            };
+
+            // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+            jQuery.post(ajaxurl, data, function(response) {
+                alert('Got this from the server: ' + response);
+            });
+        });
+    </script> --><?php
+/*}*/
